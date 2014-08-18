cf0c6f48a0f92e5b81ec8cbfe8d8b028d40fc3f4
Jonathan Vollebregt <jonathan-vola@hotmail.com>
2014-08-19 00:43:12 +0200

    Yet again - brand new design. More work, but less clunky

--- cobjects.h
+++ cobjects.h
@@ -1,16 +1,25 @@
+#ifndef __cObjects_H
+#define __cObjects_H
+
 #include <stdlib.h>
 
-#define CLASS() typedef struct {
-#define SUBCLASS(T) typedef struct { T parent;
+/* Used to define base class in struct. Must be first element of struct. */
+#define INHERIT(T) T __cObjects_parent
 
+/* For easy casting between objects */
 #define CAST(A,B) ((B *)A)
-#define OALLOC(T) malloc(sizeof(T))
 
-#define CONSTRUCTOR(T,...) T * T##_new(__VA_ARGS__){ T * this = OALLOC(T);
-#define DESTRUCTOR(T) void T##_destroy(T * this){
+/* Constructor, destructor */
+#define CONSTR(T,...) T * T##_new(T * this, ##__VA_ARGS__)
+#define DESTR(T) void T##_destroy(T * this)
 
-#define P_NEW(T,...) do{ T * p = T##_new(__VA_ARGS__); this->parent = *p; free(p) ;}while(0)
-#define P_DELETE(T) T##_destroy((T *)this)
+/* NEW(T,...) and DELETE(P,T) for object creation, deletion */
+#define NEW(T,...) T##_new(malloc(sizeof(T)), ##__VA_ARGS__)
+#define DEL(P,T) do{ T##_destroy(P); free(P); }while(0)
 
-#define NEW(T,...) T##_new(__VA_ARGS__)
-#define DELETE(V,T) do{ T##_destroy(V); free(V); }while(0)
+/* P_CONSTR(T,...) and P_DESTR(T) for use in constructors to initialize the
+ * parent object */
+#define P_CONSTR(T,...) T##_new(CAST(this,T), ##__VA_ARGS__)
+#define P_DESTR(T) T##_destroy(CAST(this,T))
+
+#endif
--- implementation.c
+++ implementation.c
@@ -3,81 +3,104 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Ignore the lack of opening curly brace. It's a limitation of the
- * C macro system */
 
-// firstClass
-CLASS()
+typedef struct {
   int a;
 } firstClass;
 
-CONSTRUCTOR(firstClass)
+CONSTR(firstClass){
   return this;
 }
 
-DESTRUCTOR(firstClass)
+DESTR(firstClass){
+  (void) this; // Suppress unused variable warning
 }
 
 
-// initClass
-SUBCLASS(firstClass)
-  char * b;
-} initClass;
+typedef struct {
+  INHERIT(firstClass);
+  char * string;
+} textClass;
 
-CONSTRUCTOR(initClass, const char * string)
-  P_NEW(firstClass);
-  this->b = strdup(string); // Note that new memory has been allocated!
+CONSTR(textClass, const char * string){
+  // Call parent constructor on this struct
+  P_CONSTR(firstClass);
+
+  // Note that new memory has been allocated by strdup()
+  this->string = strdup(string);
+
+  // Access inherited class members by explicit cast
   CAST(this,firstClass)->a = 3;
+
+  // And finally return
   return this;
 }
 
-DESTRUCTOR(initClass)
-  free(this->b);
-  P_DELETE(firstClass);
+DESTR(textClass){
+  // Remember to delete things you allocated in the constructor
+  free(this->string);
+
+  /* Finally, always call destructor on parent class, even if it didn't allocate
+   * anything. It may have parent classes that did */
+  P_DESTR(firstClass);
 }
 
 
-// lastClass
-SUBCLASS(initClass)
-  int c;
+typedef struct {
+  INHERIT(textClass);
+  int b;
 } lastClass;
 
-CONSTRUCTOR(lastClass)
-  P_NEW(initClass, "Take a look at this string");
+CONSTR(lastClass){
+  // Calling parent constructor with arguments
+  P_CONSTR(textClass, "Take a look at this string");
+
+  printf("this.string is currently: %s\n", CAST(this,textClass)->string);
+
+  /* Lets imagine we want to override the string at this point. Free the old one
+   * allocate some new space and point to it, then copy in a new string. Note
+   * that if you're using some kind of object to represent your string you
+   * should delete it via whatever method it expects (For example if this string
+   * were a cObject: DEL(CAST(this,initClass)->string) */
+  free(CAST(this,textClass)->string);
+  CAST(this,textClass)->string = malloc(20);
+  strcpy(CAST(this,textClass)->string,"Whoop dee doo");
+
   return this;
 }
 
-DESTRUCTOR(lastClass)
-  P_DELETE(initClass);
+DESTR(lastClass){
+  /* Notice how we don't free the string we allocated in the constructor! We've
+   * already done our part by freeing the string that was there in the first
+   * place. The parent destructor will deallocate "Whoop dee doo" */
+  P_DESTR(textClass);
 }
 
 
-// Demonstration
 int main(){
   // Simple data objects are just structs
   firstClass * F = NEW(firstClass);
   F->a = 12;
   printf("F.a == %d\n",F->a);
-  DELETE(F,firstClass);
-  
+  DEL(F,firstClass);
+
   // Derived object with constructor
   lastClass * special = NEW(lastClass);
-  
-  printf( "special.a defaulted to: %d\nspecial.b defaulted to: %s\nspecial.c was not set: %d\n",
-          CAST(special,firstClass)->a,
-          CAST(special,initClass)->b,
-          special->c
-        );
-        
-  char * s = CAST(special,initClass)->b;
+
+  printf(
+    "special.a defaulted to: %d\nspecial.string defaulted to: %s\nspecial.b was not set: %d\n",
+    CAST(special,firstClass)->a,
+    CAST(special,textClass)->string,
+    special->b
+  );
+
+  char * s = CAST(special,textClass)->string;
   int * a = &CAST(special,firstClass)->a;
-  printf("s now points to: %p which is: %s\na now points to %p which is %d\n",s,s,a,*a);
-  
-  DELETE(special,lastClass);
-  
-  printf("s still points to: %p which is now: %s\na still points to %p which is still: %d\n",s,s,a,*a);
-  
-  printf("a still points to %p which is now: %d\n",a,*a);
-  
+  printf(
+    "s now points to: %p which is: %s\na now points to %p which is %d\n",
+    s, s, a, *a);
+
+  DEL(special,lastClass);
+
   return 0;
 }
